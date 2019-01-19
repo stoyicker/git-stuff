0f07e05d1ddbabd139cdcea30b56267fa28619a7
Jorge Antonio Díaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-01-19 09:19:58 +0100

    Replace $() by ``

--- config.sh
+++ config.sh
@@ -28,4 +28,4 @@ git config --global alias.assume-unchanged "update-index --assume-unchanged"
 git config --global alias.unassume-unchanged "update-index --no-assume-unchanged"
 git config --global alias.commitfortag "rev-list -n 1"
 # Call like this: git deepcopy remoturl
-git config --global alias.deepcopy '!f() { curl https://raw.githubusercontent.com/stoyicker/git-stuff/master/deepcopy | bash -s $1 $(echo $1 | grep -oP '(?<=/)[^/.]+') ; }; f'
+git config --global alias.deepcopy '!f() { curl https://raw.githubusercontent.com/stoyicker/git-stuff/master/deepcopy | bash -s $1 `echo $1 | grep -oP '(?<=/)[^/.]+'`; }; f'
